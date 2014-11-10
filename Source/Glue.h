a8a8623c27a3f69f5bc24be378d282bd59ba881d
Dominique d'Argent <dargent@in.tum.de>
2014-11-10 20:46:47 +0100

    Init Xcode project

---
+++ Source/Glue.h
@@ -0,0 +1,19 @@
+//
+//  Glue.h
+//  Glue
+//
+//  Created by Dominique d'Argent on 10/11/14.
+//  Copyright (c) 2014 Dominique d'Argent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Glue.
+FOUNDATION_EXPORT double GlueVersionNumber;
+
+//! Project version string for Glue.
+FOUNDATION_EXPORT const unsigned char GlueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Glue/PublicHeader.h>
+
+
